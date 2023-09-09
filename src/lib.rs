1cf17992eacf036f0084f2419ac07322cf592407
Barugon <16503728+Barugon@users.noreply.github.com>
2023-09-09 16:05:40 -0700

    Add comment about `edit_focus` in debug info

--- src/lib.rs
+++ src/lib.rs
@@ -86,6 +86,7 @@ impl Debug for FileDialog {
       .field("anchor", &self.anchor)
       // Closures don't implement std::fmt::Debug.
       // .field("filter", &self.filter)
+      // .field("edit_focus", &self.edit_focus)
       .field("resizable", &self.resizable)
       .field("rename", &self.rename)
       .field("new_folder", &self.new_folder)
@@ -108,6 +109,7 @@ impl Debug for FileDialog {
       .field("anchor", &self.anchor)
       // Closures don't implement std::fmt::Debug.
       // .field("filter", &self.filter)
+      // .field("edit_focus", &self.edit_focus)
       .field("resizable", &self.resizable)
       .field("rename", &self.rename)
       .field("new_folder", &self.new_folder)
@@ -130,6 +132,7 @@ impl Debug for FileDialog {
       .field("anchor", &self.anchor)
       // Closures don't implement std::fmt::Debug.
       // .field("filter", &self.filter)
+      // .field("edit_focus", &self.edit_focus)
       .field("resizable", &self.resizable)
       .field("rename", &self.rename)
       .field("new_folder", &self.new_folder)
